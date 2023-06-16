aaa685bcca1111bc13159d8ecf53db1cc63266dc
CharlesGraczcki <117951262+CharlesGraczcki@users.noreply.github.com>
2023-06-16 13:43:11 -0300

    atualizado

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-echo "\n OLÁ,EU SOU O BOT DE ATENDIMENTO! \n ";
+# echo "\n OLÁ,EU SOU O BOT DE ATENDIMENTO! \n ";
 
 $servidor = 'localhost';
 $usuario = 'root';
 $senha = '';
-$banco = 'bot';
+$banco = 'bot_curso';
 $conn = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 if (!$conn) {
-    echo "\n Conexão concluida!";
+    #echo "\n Conexão concluida!";
 } else {
-    echo "\n Conexão não concuida :/  ";
+    echo "\n Conexão não concuida ". mysqli_error($conn);
 }
 
 $nome = "victor";
@@ -23,15 +23,12 @@ $query = mysqli_query($conn, $sql);
 if (!$query) {
     echo " \n erro ao inserir os dados:\n  " . mysqli_error($conn);
 } else {
-    echo "\n deu tudo certo\n ";
+# echo "\n deu tudo certo\n ";
 }
-?>
-<?php
-$sql = "SELECT * FROM bot WHERE nome = 'victor'";
-$query = mysqli_query($conn, $sql);
-
-while ($row_usuarios = mysqli_fetch_array($query)) {
-    $nome = $row_usuarios['nome'];
-    echo $nome;
-}
+###########################
+#VARIAVEIS IMPORTANTES 
+$numero_telefone = $_GET['telefone'];
+$msg = $_GET['msg'];
+$usuario = $_GET ['usuario'];
+echo "*telefone* $numero_telefone *MSG* $msg *usuario* $usuario";
 ?>
